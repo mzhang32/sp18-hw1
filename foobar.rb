e7fd1fa48abc9392584818843d012dced9a42aa1
Michelle Zhang <mzhang32@gmail.com>
2018-02-14 00:52:18 -0800

    stuff works

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,7 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    b = a.map{|s| s.to_i+2 }.uniq
+    b.select{|n| n % 2 == 0 && n < 10}.sum
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort.map { |a| a+1 }
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  "#{first_name}#{" "}#{last_name}"
 end
 
 def blockin_time(a)
@@ -49,4 +51,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.split("").map{ |c| values[:"#{c}"]}.sum
 end
